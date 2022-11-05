fdf5056f7e9dd7d12bae1951dc0bcb8398b50177
Ryuk-C <cumahaznedar@gmail.com>
2022-11-05 14:54:44 +0100

    Initial Commit

---
+++ ImageSlider/ImageSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageSliderApp.swift
+//  ImageSlider
+//
+//  Created by Cuma on 05/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct ImageSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
